fa0e455113a88170d0b6aa1324626c5b2696f927
Collins Kipkemoi <chemerilcollins@gmail.com>
2024-08-19 01:32:24 +0200

    Feat: Add routes and create pages

--- Frontend/Authify/src/App.tsx
+++ Frontend/Authify/src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Profile from "./pages/Profile"
+import About from "./pages/About"
 function App() {
   return (
-    <div>App</div>
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
