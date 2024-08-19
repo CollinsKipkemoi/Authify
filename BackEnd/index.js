6b87c72df18ec977d03b9a3bbe3001d17857ca25
Collins Kipkemoi <chemerilcollins@gmail.com>
2024-08-19 12:38:18 +0200

    Feat: Connect to Mongo

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
 app.use(express.json());
+dotenv.config();
 
 app.get("/api", (req, res) => {
   res.send({
@@ -11,7 +14,11 @@ app.get("/api", (req, res) => {
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-    console.log("Listening on port", PORT);
-    
+mongoose.connect(
+  process.env.MONGO
+).then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 })
